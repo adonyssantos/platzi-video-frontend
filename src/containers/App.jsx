f9f0489fe1dfb8f0ae62bfe16ad2f5ad876e3c3a
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-29 19:25:42 -0400

    Add hooks to get data from fake API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,6 +8,18 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 export const App = () => {
+  const API = 'http://localhost:3000/initalState';
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => setVideos(data))
+      .catch(error => console.error(error.message));
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className='App'>
       <Header />

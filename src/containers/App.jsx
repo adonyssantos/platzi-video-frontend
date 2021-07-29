2e36475ab5ccfb03530a855ba0f382426ab90e9d
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-29 17:29:41 -0400

    Add Categories, Carousel and CarouselItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 import '../assets/styles/App.scss';
 
 export const App = () => {
@@ -8,6 +11,11 @@ export const App = () => {
     <div className='App'>
       <Header />
       <Search />
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
     </div>
   );
 };

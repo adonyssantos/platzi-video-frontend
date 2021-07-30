77bfe7741ce5501193bf1968df5e7e17f469b46f
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-30 10:57:25 -0400

    Add map for mylist and originals

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -20,7 +20,9 @@ export const App = () => {
       {initialState.mylist.length > 0 && (
         <Categories title='My list'>
           <Carousel>
-            <CarouselItem />
+            {initialState.mylist.map(item => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
           </Carousel>
         </Categories>
       )}
@@ -35,7 +37,9 @@ export const App = () => {
 
       <Categories title='PlatziVideo Originals'>
         <Carousel>
-          <CarouselItem />
+          {initialState.originals.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
 

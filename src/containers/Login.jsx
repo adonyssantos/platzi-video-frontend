f6598ce0f0f02385ed5d9946874c00d3c410eb0f
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-02 11:53:23 -0400

    Add Login container

---
+++ src/containers/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import googleIcon from '../assets/static/google-icon.png';
+import twitterIcon from '../assets/static/twitter-icon.png';
+import '../assets/styles/Login.scss';
+
+const Login = () => (
+  <section className='login'>
+    <section className='login__container'>
+      <h2>Sign in</h2>
+      <form className='login__container--form'>
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <button className='button'>Sign in</button>
+        <div className='login__container--remember-me'>
+          <label>
+            <input type='checkbox' id='cbox1' value='first_checkbox' />
+            Remember me
+          </label>
+          <a href='/'>I forgot my password</a>
+        </div>
+      </form>
+      <section className='login__container--social-media'>
+        <div>
+          <img src={googleIcon} /> Sign in with Google
+        </div>
+        <div>
+          <img src={twitterIcon} /> Sign in with Twitter
+        </div>
+      </section>
+      <p className='login__container--register'>
+        I don't have an account <a href=''>Register</a>
+      </p>
+    </section>
+  </section>
+);
+
+export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,9 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',

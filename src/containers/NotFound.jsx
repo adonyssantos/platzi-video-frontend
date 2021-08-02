70dee50d3e3f68bc6a309a5e08908f3d579aa572
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-02 12:26:52 -0400

    Add 404 Not Found container

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/NotFound.scss';
+
+const NotFound = () => (
+  <section className='container__not-found'>
+    <h1 className='container__not-found--title'>404</h1>
+    <h2 className='container__not-found--subtitle'>Page not found</h2>
+  </section>
+);
+
+export default NotFound;

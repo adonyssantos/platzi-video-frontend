816345fe1ddeec3136f60401e7185f8997bd15bf
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-03 23:27:59 -0400

    Add Register with Redux

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 import '../assets/styles/Register.scss';
 
-const Register = () => {
+const Register = props => {
   const [form, setValues] = useState({
     email: '',
     name: '',
@@ -18,6 +20,8 @@ const Register = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
     console.log(form);
   };
 
@@ -57,4 +61,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -26,6 +26,11 @@ const reducer = (state, action) => {
         ...state,
         user: action.payload,
       };
+    case 'REGISTER_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }

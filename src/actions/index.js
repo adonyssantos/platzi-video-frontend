9d4393d804cfc66c49e88332687eeefbc6748d6b
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-03 21:19:10 -0400

    Add deleteFavorite

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const setFavorite = payload => ({
   type: 'SET_FAVORITE',
   payload,
 });
+
+export const deleFavorite = payload => ({
+  type: 'DELETE_FAVORITE',
+  payload,
+});
--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setFavorite } from '../actions';
+import { setFavorite, deleFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = props => {
-  const { cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration } = props;
   const handleSetFavorite = () => {
-    props.setFavorite({ cover, title, year, contentRating, duration });
+    props.setFavorite({ id, cover, title, year, contentRating, duration });
+  };
+
+  const handleDeleteFavorite = itemId => {
+    props.deleFavorite(itemId);
   };
 
   return (
@@ -27,6 +32,12 @@ const CarouselItem = props => {
             src={plusIcon}
             alt='Plus Icon'
             onClick={handleSetFavorite}
+          />
+          <img
+            className='carousel-item__details--img'
+            src={removeIcon}
+            alt='Remove Icon'
+            onClick={() => handleDeleteFavorite(id)}
           />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
@@ -48,6 +59,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
   setFavorite,
+  deleFavorite,
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);

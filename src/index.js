ab5a3fa10c146e942e583fce9a6721a6a36407bf
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-03 21:08:33 -0400

    Add setFavorite

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { initialState } from '../initialState.json';
 import reducer from './reducers';
 import App from './routes/App';
-import { initialState } from '../initialState.json';
 
 const store = createStore(reducer, initialState);
 

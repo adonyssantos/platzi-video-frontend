5b47b9eb8da57ae837183e2afe2196bd5bf4da34
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-05 21:45:18 -0400

    Add Player general view

---
+++ src/containers/Player.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../assets/styles/Player.scss';
+
+const Player = props => {
+  return (
+    <div className='Player'>
+      <video controls autoplay>
+        <source src='' type='video/mp4' />
+      </video>
+      <div className='Player-back'>
+        <button type='button'>Back</button>
+      </div>
+    </div>
+  );
+};
+
+Player.propTypes = {};
+
+export default Player;
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
-import NotFound from '../containers/NotFound.jsx';
+import NotFound from '../containers/NotFound';
+import Player from '../containers/Player';
 import Layout from '../components/Layout';
 
 const App = () => (
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/player/:id' component={Player} />
         <Route component={NotFound} />
       </Switch>
     </Layout>

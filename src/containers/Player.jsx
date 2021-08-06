2a0c657b9bcab5ed8d21d6f185f8b563ea6614da
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-05 23:19:02 -0400

    Update Player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import { Redirect } from 'react-router-dom';
 import '../assets/styles/Player.scss';
 
 const Player = props => {
-  return (
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='Player'>
-      <video controls autoplay>
-        <source src='' type='video/mp4' />
+      <video controls autoPlay>
+        <source src={props.playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
         <button type='button' onClick={() => props.history.goBack()}>
@@ -14,9 +24,21 @@ const Player = props => {
         </button>
       </div>
     </div>
+  ) : (
+    <Redirect to='/404/' />
   );
 };
 
 Player.propTypes = {};
 
-export default Player;
+const mapStateToProps = state => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -31,6 +31,15 @@ const reducer = (state, action) => {
         ...state,
         user: action.payload,
       };
+    case 'GET_VIDEO_SOURCE':
+      const videos = [...state.trends, ...state.originals];
+      return {
+        ...state,
+        playing: videos.find(item => item.id === Number(action.payload)) || [],
+        // state.trends.find(item => item.id === Number(action.payload)) ||
+        // state.originals.find(item => item.id === Number(action.payload)) ||
+        // [],
+      };
     default:
       return state;
   }

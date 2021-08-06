94f04c1c44b9795df56434d275014a1fd5c1632c
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-06 00:07:17 -0400

    Fix load bug

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getVideoSource } from '../actions';
-import { Redirect } from 'react-router-dom';
+import NotFound from './NotFound';
+// import { Redirect } from 'react-router-dom';
 import '../assets/styles/Player.scss';
 
 const Player = props => {
@@ -25,7 +26,8 @@ const Player = props => {
       </div>
     </div>
   ) : (
-    <Redirect to='/404/' />
+    <NotFound />
+    // <Redirect to='/404/' />
   );
 };
 

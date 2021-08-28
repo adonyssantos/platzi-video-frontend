60aab7b4ae60bdcae82367974f2bb28550f1d7a7
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-27 21:10:49 -0400

    Serve React with Express

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,25 @@ if (config.dev) {
 }
 
 app.get('*', (request, response) => {
-  response.send({ name: 'Adonys' });
+  const content = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Platzi Video with ReactJS" />
+      <title>Platzi Video</title>
+      <link href="assets/app.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+      <div id="app"></div>
+      <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+  </html>
+  `;
+
+  response.send(content);
 });
 
 app.listen(config.port, error => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -11,7 +10,7 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'assets/app.js',
     publicPath: '/',
   },
   resolve: {
@@ -60,12 +59,8 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
+      filename: 'assets/app.css',
     }),
   ],
 };

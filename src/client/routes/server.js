9b37283492f228a184997c0229cccebbfffaa863
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-29 14:28:53 -0400

    Abstract React Router

---
+++ src/client/routes/server.js
@@ -0,0 +1,30 @@
+import { Home, Login, NotFound, Player, Register } from '../containers';
+
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: Home,
+  },
+  {
+    exact: true,
+    path: '/login',
+    component: Login,
+  },
+  {
+    exact: true,
+    path: '/register',
+    component: Register,
+  },
+  {
+    exact: true,
+    path: '/player/:id',
+    component: Player,
+  },
+  {
+    name: 'NotFound',
+    component: NotFound,
+  },
+];
+
+export default routes;

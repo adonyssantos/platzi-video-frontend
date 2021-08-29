1bd23d82ef8a614dcbba83068252cde8141b9a49
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-29 14:28:22 -0400

    Add history

--- src/client/index.js
+++ src/client/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import { initialState } from '../../initialState.json';
 import reducer from './reducers';
 import App from './routes/App';
 
+const history = createBrowserHistory();
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(reducer, initialState, composeEnhancers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app'),
 );

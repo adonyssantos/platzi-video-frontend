d88cf4532992b8136b694edb8cbab385c26155e1
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-30 10:57:36 -0400

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: [
-    './src/client/index.js',
+require('dotenv').config();
+
+const isDev = process.env.ENV === 'development';
+const entry = ['./src/client/index.js'];
+
+if (isDev) {
+  entry.push(
     'webpack-hot-middleware/client?path=/__webpack_ hmr&timeout=2000&reload=true',
-  ],
-  mode: 'development',
+  );
+}
+
+module.exports = {
+  entry,
+  mode: process.env.ENV,
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './src/server/public'),
     filename: 'assets/app.js',
     publicPath: '/',
   },
@@ -58,7 +66,7 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
     new MiniCssExtractPlugin({
       filename: 'assets/app.css',
     }),

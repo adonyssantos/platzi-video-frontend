3d38e8508514ab5bf24040ad7ab697bd0da8f43a
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-09-02 17:24:58 -0400

    Add nextjs

---
+++ nextjs/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <div>Hello world</div>;
+};
+
+export default Landing;
---
+++ nextjs/pages/platzi.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <div>Hello Platzi</div>;
+};
+
+export default Landing;
